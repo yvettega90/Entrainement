d5b79de681623bc4bade5624e74d468527d53d04
yvettega90 <yvettegaelle26@gmail.com>
2023-02-22 13:57:58 +0100

    end

--- exo4/index4.js
+++ exo4/index4.js
@@ -1,6 +1,10 @@
 //exo4 
-let dateOfBirth = "21/12/2000";
+let dateOfBirth = 2000;
 let name = "Elsa"
-let currentAge = 
+let yearCurrent = 2023;
+let currentAge = yearCurrent - dateOfBirth
+let a2028 = 2028;
+let age2028 = a2028 - dateOfBirth
 
-console.log("Bonjour je m'appelle Elsa. J'ai x ans.Le 21/12/2028 j'aurai x ans ");
+
+console.log(" Bonjour je m'appelle " + name  + " J'ai " + currentAge + " ans " + " Le 21/12/2028 j'aurai " + age2028 + " ans " );
